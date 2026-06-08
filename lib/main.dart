2f8abae0b87f5d8deaf718af53d2b869536881a9
CriCri-Tech <cristianlamesta08@gmail.com>
2026-06-08 11:39:03 +0200

    Modifica al main con collegamento firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
-// 1. IMPORTA LA TUA PAGINA
-import '/presentazione/schermate/dashboard_page.dart'; 
+// IMPORTA IL PACCHETTO CORE DI FIREBASE
+import 'package:firebase_core/firebase_core.dart'; 
 
-void main() {
-  // Il punto di partenza assoluto dell'applicazione
+import 'presentazione/schermate/dashboard_page.dart'; 
+
+
+void main() async {
+  // Assicura che i canali nativi di Flutter siano pronti
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // INIZIALIZZA FIREBASE PRIMA DI FAR PARTIRE L'APP
+  await Firebase.initializeApp();
+
+  // Quando firebase è pronto fa partire l'applicazione
   runApp(const MiaApp());
 }
 
@@ -14,12 +23,11 @@ class MiaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nexa',
-      debugShowCheckedModeBanner: false, // Rimuove la striscia rossa "Debug" in alto a destra sul telefono
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E3A8A)),
-        useMaterial3: true, // Attiva il design Material 3 (consigliato)
+        useMaterial3: true,
       ),
-      // 2. IMPOSTA LA TUA PAGINA COME SCHERMATA INIZIALE
       home: const RegistrazionePage(), 
     );
   }
