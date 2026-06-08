1ec58475959a040e5a54b1989afa4ab0ff1ae2f3
tassy23 <smoksy23@gmail.com>
2026-06-08 10:15:06 +0200

    Aggiunta del RF-06 - Gestione clienti

---
+++ lib/modelli/cliente.dart
@@ -0,0 +1,61 @@
+// Pacchetto che permette di sinctronizzare i dati tra l'app e il database in tempo reale
+import "package:cloud_firestore/cloud_firestore.dart";
+
+// Classe che rappresenta il singolo cliente
+class Cliente {
+  String? id; // ID univoco del cliente
+  String companyName; // Nome dell'azienda
+  String vatNumber; // Partita IVA
+  String taxCode; // Codice fiscale
+  String pec;
+  String sdiCode;
+  String phone;
+  String email;
+  String address;
+  DateTime createdAt; // Data di creazione del cliente
+
+  // Metodo costruttore
+  Cliente({
+    this.id,
+    required this.companyName,
+    required this.vatNumber,
+    required this.taxCode,
+    required this.pec,
+    required this.sdiCode,
+    required this.phone,
+    required this.email,
+    required this.address,
+    required this.createdAt, 
+  });
+
+  // Metodo per convertire un documento Firestore in un oggetto Cliente
+  factory Cliente.fromMap(Map<String, dynamic> data, String documentId) {
+    return Cliente(
+      id: documentId,
+      companyName: data['companyName'] ?? '',
+      vatNumber: data['vatNumber'] ?? '',
+      taxCode: data['taxCode'] ?? '',
+      pec: data['pec'] ?? '',
+      sdiCode: data['sdiCode'] ?? '',
+      phone: data['phone'] ?? '',
+      email: data['email'] ?? '',
+      address: data['address'] ?? '',
+      createdAt: (data['createdAt'] as Timestamp).toDate(),
+    );
+  }
+
+  // Metodo per convertire un oggetto Cliente in una mappa da salvare su Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'companyName': companyName,
+      'vatNumber': vatNumber,
+      'taxCode': taxCode,
+      'pec': pec,
+      'sdiCode': sdiCode,
+      'phone': phone,
+      'email': email,
+      'address': address,
+      'createdAt': Timestamp.fromDate(createdAt),
+    };
+  }
+}
