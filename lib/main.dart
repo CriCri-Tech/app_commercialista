44c69b1bb364aa05eb515a6b40b35d2edfee8e61
CriCri-Tech <cristianlamesta08@gmail.com>
2026-06-08 11:32:03 +0200

    Sistemazione del main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
-// IMPORTA IL PACCHETTO CORE DI FIREBASE
-import 'package:firebase_core/firebase_core.dart'; 
+// 1. IMPORTA LA TUA PAGINA
+import '/presentazione/schermate/dashboard_page.dart'; 
 
-// IMPORTA LA NUOVA LANDING PAGE
-// Nota: adatta questo percorso in base a dove hai salvato il file landing_page.dart
-import 'presentazione/schermate/welcome_page.dart'; 
-
-void main() async {
-  // Assicura che i canali nativi di Flutter siano pronti
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // INIZIALIZZA FIREBASE PRIMA DI FAR PARTIRE L'APP
-  await Firebase.initializeApp();
-
-  // Quando Firebase è pronto fa partire l'applicazione
+void main() {
+  // Il punto di partenza assoluto dell'applicazione
   runApp(const MiaApp());
 }
 
@@ -24,13 +14,13 @@ class MiaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nexa',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, // Rimuove la striscia rossa "Debug" in alto a destra sul telefono
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E3A8A)),
-        useMaterial3: true,
+        useMaterial3: true, // Attiva il design Material 3 (consigliato)
       ),
-      //Imposta la WelcomePage come prima schermata all'avvio
-      home: const WelcomePage(), 
+      // 2. IMPOSTA LA TUA PAGINA COME SCHERMATA INIZIALE
+      home: const RegistrazionePage(), 
     );
   }
 }
