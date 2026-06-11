568b92e7338b89bb40465e2a80976d999ca0b45d
CriCri-Tech <cristianlamesta08@gmail.com>
2026-06-11 09:19:50 +0200

    Ho sistemado le dipendenze

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,13 +15,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.nexa"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -45,4 +44,9 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+// blocco delle dipendenze specifico per Kotlin DSL
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
